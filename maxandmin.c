ec57f8ce5b95faa5b37c51908c92a794ee8b4a64
Harshitha49 <120377721+Harshitha49@users.noreply.github.com>
2023-01-29 10:43:18 -0800

    Add files via upload

---
+++ maxandmin.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,num3;
+printf("enter num1,num2,num3");
+scanf("%d%d%d",&num1,&num2,&num3);
+if(num1>num2);
+{
+if(num1>num3)
+printf("%d",num1);
+else
+printf("%d",num3);
+}
+{
+if(num2>num3)
+printf("%d",num2);
+else
+if("%d",num3);
+}
+return;
+}
